deccfcf5792febd628b7f0d85b26eb3e14e5288f
alvaromch <alvaromch1510@gmail.com>
2022-07-28 11:23:29 -0400

    Estructura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: InicioComponent
+      },
+      {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+      }
+    ]
+  },
+  {
+    path: 'sisebs',
+    loadChildren: () => import('./sisebs/sisebs.module').then(m => m.SisebsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/sisebs/sisebs.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SisebsRoutingModule } from './sisebs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SisebsRoutingModule
+  ]
+})
+export class SisebsModule { }
